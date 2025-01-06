636c081d9138f628bceecdeff36b589955a3599f
Arif Ramadhani <129027469+ArifRTzy@users.noreply.github.com>
2025-01-06 14:07:05 +0700

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -3,18 +3,18 @@ import mongoose from "mongoose";
 let isConnected = false
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true)
+    mongoose.set('strictQuery', true)//will throw errors when the query is incorrect e.g "emial". If set to false the errors will be get ignored
 
     if(isConnected){
         console.log('MongoDB is already connected')
-        return;
+        return;//return will stops the function imediately without the return the code below will executed
     }
 
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "share-promt",
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+        await mongoose.connect(process.env.MONGODB_URI, {//use to connect to the mongoDB database
+            dbName: "share-promt",//database name
+            useNewUrlParser: true,//Ensures Mongoose uses the latest connection string parser for compatibility with MongoDB 4+.
+            useUnifiedTopology: true,//Enables the new unified topology layer, improving connection stability, performance, and behavior consistency.
         })
 
         isConnected = true
